b4093649b9bf4c3db4a693c47e672a5fae0ee291
rumaisharaghibsrg <rumaisharaghib@gmail.com>
2026-02-08 11:26:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function downloadCV() {
+    window.open('CV.pdf', '_blank');
+}
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
